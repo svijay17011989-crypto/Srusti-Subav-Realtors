9fa4c7f785b4eca5c73edcc889d958dd60b1de54
Vijay <s.vijay17011989@gmail.com>
2025-12-17 23:37:33 +0530

    Fix server paths and start script

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend static files
-app.use(express.static(path.join(__dirname, "..", "frontend")));
+app.use(express.static(path.join(__dirname, "frontend")));
 
 // Cloudinary Configuration
 cloudinary.config({
